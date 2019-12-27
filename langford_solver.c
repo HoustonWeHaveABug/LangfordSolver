a6c67dae662da4a40704cf47433e24226e012f1b
HoustonWeHaveABug <HoustonWeHaveABug@users.noreply.github.com>
2019-12-27 18:30:03 +0100

    v2.0
    Changed processing order of rows when in "first only" mode to optimize execution time.
    Refreshed achievement text files.

--- langford_solver.c
+++ langford_solver.c
@@ -37,15 +37,17 @@ mp_t;
 unsigned long get_group_choices(unsigned long);
 unsigned long get_group_half_choices(unsigned long);
 void set_column(node_t *, node_t *);
-void add_group_strict_half_choices(unsigned long, unsigned long, unsigned long);
-void add_group_choices(unsigned long, unsigned long, unsigned long);
-void add_group_half_choices(unsigned long, unsigned long, unsigned long);
-void add_group_choice(unsigned long, unsigned long, unsigned long);
+void add_group_strict_half_choices(unsigned long, unsigned long);
+void add_group_choices(unsigned long, unsigned long);
+void add_group_half_choices(unsigned long, unsigned long);
 void set_choice_cur(unsigned long, unsigned long);
-void set_row_node(node_t *, node_t *, node_t **);
+int compare_choices(const void *, const void *);
+void add_row_nodes(choice_t *);
+void set_row_node(node_t *, choice_t *, node_t *, node_t **);
 void link_left(node_t *, node_t *);
 void link_top(node_t *, node_t *);
 void dlx_search(void);
+void assign_row(node_t *);
 void assign_choice(choice_t *);
 void cover_column(node_t *);
 void uncover_column(node_t *);
@@ -56,14 +58,14 @@ int mp_eq_val(mp_t *, unsigned long);
 void mp_free(mp_t *);
 
 int option_first_only, option_verbose;
-unsigned long order, sequence_size, *sequence;
+unsigned long order, range_inf, sequence_size, *sequence;
 choice_t *choice_cur;
-node_t *nodes, **tops, *header, *row_node;
+node_t **tops, *nodes, *header, *row_node;
 mp_t cost, solutions_n;
 
 int main(void) {
 	int options;
-	unsigned long intervals_n, range_inf, range_sup, groups_n, hooks_n, columns_n, group_sup_choices_n, group_choices_n, hook_choices_n, nodes_n, i;
+	unsigned long intervals_n, range_sup, groups_n, hooks_n, columns_n, group_sup_choices_n, group_choices_n, hook_choices_n, nodes_n, i;
 	choice_t *choices;
 	if (scanf("%lu", &order) != 1 || order < ORDER_MIN) {
 		fprintf(stderr, "Order must be greater than or equal to %lu\n", ORDER_MIN);
@@ -123,6 +125,65 @@ int main(void) {
 		return EXIT_FAILURE;
 	}
 	group_sup_choices_n = get_group_choices(intervals_n*range_sup);
+	group_choices_n = group_sup_choices_n/2UL;
+	for (i = range_sup-1UL; i >= range_inf; i--) {
+		group_choices_n += get_group_choices(intervals_n*i);
+	}
+	if (hooks_n > 0UL) {
+		hook_choices_n = sequence_size;
+	}
+	else {
+		hook_choices_n = 0UL;
+	}
+	choices = malloc(sizeof(choice_t)*(group_choices_n+hook_choices_n));
+	if (!choices) {
+		fprintf(stderr, "Error allocating memory for choices\n");
+		fflush(stderr);
+		mp_free(&solutions_n);
+		mp_free(&cost);
+		free(tops);
+		free(sequence);
+		return EXIT_FAILURE;
+	}
+	nodes_n = columns_n+1UL+group_choices_n*(order+1UL)+hook_choices_n;
+	nodes = malloc(sizeof(node_t)*nodes_n);
+	if (!nodes) {
+		fprintf(stderr, "Error allocating memory for nodes\n");
+		fflush(stderr);
+		free(choices);
+		mp_free(&solutions_n);
+		mp_free(&cost);
+		free(tops);
+		free(sequence);
+		return EXIT_FAILURE;
+	}
+	header = nodes+columns_n;
+	set_column(nodes, header);
+	for (i = 0UL; i < columns_n; i++) {
+		set_column(nodes+i+1UL, nodes+i);
+		tops[i] = nodes+i;
+	}
+	choice_cur = choices;
+	row_node = nodes+columns_n+1UL;
+	add_group_strict_half_choices(range_sup, intervals_n*range_sup);
+	for (i = range_sup-1UL; i >= range_inf; i--) {
+		add_group_choices(i, intervals_n*i);
+	}
+	qsort(choices, group_choices_n, sizeof(choice_t), compare_choices);
+	for (i = 0UL; i < group_choices_n; i++) {
+		add_row_nodes(choices+i);
+	}
+	for (i = 0UL; i < hook_choices_n; i++) {
+		set_choice_cur(0UL, i);
+		set_row_node(nodes+i, choice_cur, row_node, tops+i);
+		choice_cur++;
+	}
+	for (i = 0UL; i < columns_n; i++) {
+		link_top(nodes+i, tops[i]);
+	}
+	dlx_search();
+	free(nodes);
+	free(choices);
 	if (group_sup_choices_n%2UL == 1UL) {
 		group_choices_n = 1UL;
 		if (range_sup > range_inf) {
@@ -172,16 +233,21 @@ int main(void) {
 		}
 		choice_cur = choices;
 		row_node = nodes+columns_n+1UL;
-		add_group_choice(range_sup, group_sup_choices_n/2UL, sequence_size+range_sup-range_inf);
+		set_choice_cur(range_sup, group_sup_choices_n/2UL);
+		choice_cur++;
 		if (range_sup > range_inf) {
-			add_group_half_choices(range_sup-1UL, intervals_n*(range_sup-1UL), sequence_size+range_sup-1UL-range_inf);
+			add_group_half_choices(range_sup-1UL, intervals_n*(range_sup-1UL));
 			for (i = range_sup-2UL; i >= range_inf; i--) {
-				add_group_choices(i, intervals_n*i, sequence_size+i-range_inf);
+				add_group_choices(i, intervals_n*i);
 			}
+		}
+		qsort(choices, group_choices_n, sizeof(choice_t), compare_choices);
+		for (i = 0UL; i < group_choices_n; i++) {
+			add_row_nodes(choices+i);
 		}
 		for (i = 0UL; i < hook_choices_n; i++) {
 			set_choice_cur(0UL, i);
-			set_row_node(nodes+i, row_node, tops+i);
+			set_row_node(nodes+i, choice_cur, row_node, tops+i);
 			choice_cur++;
 		}
 		for (i = 0UL; i < columns_n; i++) {
@@ -191,61 +257,6 @@ int main(void) {
 		free(nodes);
 		free(choices);
 	}
-	group_choices_n = group_sup_choices_n/2UL;
-	for (i = range_sup-1UL; i >= range_inf; i--) {
-		group_choices_n += get_group_choices(intervals_n*i);
-	}
-	if (hooks_n > 0UL) {
-		hook_choices_n = sequence_size;
-	}
-	else {
-		hook_choices_n = 0UL;
-	}
-	choices = malloc(sizeof(choice_t)*(group_choices_n+hook_choices_n));
-	if (!choices) {
-		fprintf(stderr, "Error allocating memory for choices\n");
-		fflush(stderr);
-		mp_free(&solutions_n);
-		mp_free(&cost);
-		free(tops);
-		free(sequence);
-		return EXIT_FAILURE;
-	}
-	nodes_n = columns_n+1UL+group_choices_n*(order+1UL)+hook_choices_n;
-	nodes = malloc(sizeof(node_t)*nodes_n);
-	if (!nodes) {
-		fprintf(stderr, "Error allocating memory for nodes\n");
-		fflush(stderr);
-		free(choices);
-		mp_free(&solutions_n);
-		mp_free(&cost);
-		free(tops);
-		free(sequence);
-		return EXIT_FAILURE;
-	}
-	header = nodes+columns_n;
-	set_column(nodes, header);
-	for (i = 0UL; i < columns_n; i++) {
-		set_column(nodes+i+1UL, nodes+i);
-		tops[i] = nodes+i;
-	}
-	choice_cur = choices;
-	row_node = nodes+columns_n+1UL;
-	add_group_strict_half_choices(range_sup, intervals_n*range_sup, sequence_size+range_sup-range_inf);
-	for (i = range_sup-1UL; i >= range_inf; i--) {
-		add_group_choices(i, intervals_n*i, sequence_size+i-range_inf);
-	}
-	for (i = 0UL; i < hook_choices_n; i++) {
-		set_choice_cur(0UL, i);
-		set_row_node(nodes+i, row_node, tops+i);
-		choice_cur++;
-	}
-	for (i = 0UL; i < columns_n; i++) {
-		link_top(nodes+i, tops[i]);
-	}
-	dlx_search();
-	free(nodes);
-	free(choices);
 	mp_print("Final cost", &cost);
 	mp_print("Solutions", &solutions_n);
 	fflush(stdout);
@@ -269,46 +280,28 @@ void set_column(node_t *node, node_t *left) {
 	link_left(node, left);
 }
 
-void add_group_strict_half_choices(unsigned long step, unsigned long group_span, unsigned long group_index) {
+void add_group_strict_half_choices(unsigned long step, unsigned long group_span) {
 	unsigned long group_choices_n = get_group_choices(group_span)/2UL, i;
-	for (i = group_choices_n; i > 0UL; i--) {
-		add_group_choice(step, i-1UL, group_index);
+	for (i = 0UL; i < group_choices_n; i++) {
+		set_choice_cur(step, i);
+		choice_cur++;
 	}
 }
 
-void add_group_choices(unsigned long step, unsigned long group_span, unsigned long group_index) {
-	unsigned long group_choices_n = get_group_choices(group_span), group_half_choices_n = group_choices_n/2UL, i;
-	if (group_choices_n%2UL == 1UL) {
-		add_group_choice(step, group_half_choices_n, group_index);
-		for (i = 1UL; i <= group_half_choices_n; i++) {
-			add_group_choice(step, group_half_choices_n-i, group_index);
-			add_group_choice(step, group_half_choices_n+i, group_index);
-		}
-	}
-	else {
-		for (i = 1UL; i <= group_half_choices_n; i++) {
-			add_group_choice(step, group_half_choices_n-i, group_index);
-			add_group_choice(step, group_half_choices_n+i-1UL, group_index);
-		}
+void add_group_choices(unsigned long step, unsigned long group_span) {
+	unsigned long group_choices_n = get_group_choices(group_span), i;
+	for (i = 0UL; i < group_choices_n; i++) {
+		set_choice_cur(step, i);
+		choice_cur++;
 	}
 }
 
-void add_group_half_choices(unsigned long step, unsigned long group_span, unsigned long group_index) {
+void add_group_half_choices(unsigned long step, unsigned long group_span) {
 	unsigned long group_choices_n = get_group_half_choices(group_span), i;
-	for (i = group_choices_n; i > 0UL; i--) {
-		add_group_choice(step, i-1UL, group_index);
+	for (i = 0UL; i < group_choices_n; i++) {
+		set_choice_cur(step, i);
+		choice_cur++;
 	}
-}
-
-void add_group_choice(unsigned long step, unsigned long start, unsigned long group_index) {
-	unsigned long i;
-	set_choice_cur(step, start);
-	set_row_node(nodes+start, row_node+order, tops+start);
-	for (i = 1UL; i < order; i++) {
-		set_row_node(nodes+start+i*step, row_node-1, tops+start+i*step);
-	}
-	set_row_node(nodes+group_index, row_node-1, tops+group_index);
-	choice_cur++;
 }
 
 void set_choice_cur(unsigned long step, unsigned long start) {
@@ -316,10 +309,36 @@ void set_choice_cur(unsigned long step, unsigned long start) {
 	choice_cur->start = start;
 }
 
-void set_row_node(node_t *column, node_t *left, node_t **top) {
+int compare_choices(const void *a, const void *b) {
+	const choice_t *choice_a = (const choice_t *)a, *choice_b = (const choice_t *)b;
+	if (choice_a->start < choice_b->start) {
+		return -1;
+	}
+	if (choice_a->start > choice_b->start) {
+		return 1;
+	}
+	if (choice_a->step < choice_b->step) {
+		return 1;
+	}
+	if (choice_a->step > choice_b->step) {
+		return -1;
+	}
+	return 0;
+}
+
+void add_row_nodes(choice_t *choice) {
+	unsigned long i;
+	set_row_node(nodes+choice->start, choice, row_node+order, tops+choice->start);
+	for (i = 1UL; i < order; i++) {
+		set_row_node(nodes+choice->start+i*choice->step, choice, row_node-1, tops+choice->start+i*choice->step);
+	}
+	set_row_node(nodes+sequence_size+choice->step-range_inf, choice, row_node-1, tops+sequence_size+choice->step-range_inf);
+}
+
+void set_row_node(node_t *column, choice_t *choice, node_t *left, node_t **top) {
 	column->rows_n++;
 	row_node->column = column;
-	row_node->choice = choice_cur;
+	row_node->choice = choice;
 	link_left(row_node, left);
 	link_top(row_node, *top);
 	*top = row_node++;
@@ -337,11 +356,8 @@ void link_top(node_t *node, node_t *top) {
 
 void dlx_search(void) {
 	unsigned long i;
-	node_t *column_min, *column, *row;
+	node_t *column_min, *column;
 	if (!mp_inc(&cost)) {
-		return;
-	}
-	if (option_first_only && mp_eq_val(&solutions_n, 1UL)) {
 		return;
 	}
 	if (header->right == header) {
@@ -359,10 +375,10 @@ void dlx_search(void) {
 		}
 		return;
 	}
-	column_min = header->right;
-	if (column_min->rows_n == 0UL) {
+	if (header->right->rows_n == 0UL) {
 		return;
 	}
+	column_min = header->right;
 	for (column = column_min->right; column != header; column = column->right) {
 		if (column->rows_n < column_min->rows_n) {
 			if (column->rows_n == 0UL) {
@@ -372,18 +388,47 @@ void dlx_search(void) {
 		}
 	}
 	cover_column(column_min);
-	for (row = column_min->bottom; row != column_min; row = row->bottom) {
-		node_t *node;
-		assign_choice(row->choice);
-		for (node = row->right; node != row; node = node->right) {
-			cover_column(node->column);
+	if (option_first_only) {
+		unsigned long half_rows_min = column_min->rows_n/2UL+column_min->rows_n%2UL;
+		node_t *middle, *top, *bottom;
+		for (i = 0UL, middle = column_min; i < half_rows_min; i++, middle = middle->bottom);
+		if (column_min->rows_n%2UL == 1UL) {
+			assign_row(middle);
+			for (top = middle->top, bottom = middle->bottom; top != column_min && mp_eq_val(&solutions_n, 0UL); top = top->top, bottom = bottom->bottom) {
+				assign_row(top);
+				if (mp_eq_val(&solutions_n, 0UL)) {
+					assign_row(bottom);
+				}
+			}
 		}
-		dlx_search();
-		for (node = row->left; node != row; node = node->left) {
-			uncover_column(node->column);
+		else {
+			for (top = middle, bottom = middle->bottom; top != column_min && mp_eq_val(&solutions_n, 0UL); top = top->top, bottom = bottom->bottom) {
+				assign_row(top);
+				if (mp_eq_val(&solutions_n, 0UL)) {
+					assign_row(bottom);
+				}
+			}
+		}
+	}
+	else {
+		node_t *row;
+		for (row = column_min->bottom; row != column_min; row = row->bottom) {
+			assign_row(row);
 		}
 	}
 	uncover_column(column_min);
+}
+
+void assign_row(node_t *row) {
+	node_t *node;
+	assign_choice(row->choice);
+	for (node = row->right; node != row; node = node->right) {
+		cover_column(node->column);
+	}
+	dlx_search();
+	for (node = row->left; node != row; node = node->left) {
+		uncover_column(node->column);
+	}
 }
 
 void assign_choice(choice_t *choice) {
